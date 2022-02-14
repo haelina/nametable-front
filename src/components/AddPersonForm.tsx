5f20c982412a76e646b51ae687cab0067dd2ba69
haelina <56834622+haelina@users.noreply.github.com>
2022-02-14 11:39:30 +0200

    Add styling to AddPersonForm

--- src/components/AddPersonForm.tsx
+++ src/components/AddPersonForm.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./AddPersonForm.css";
 
 const AddPersonForm: React.FC = () => {
   return (
-    <form className="input">
+    <form className="inputs">
       <input className="firstname" type="input" placeholder="First Name" />
       <input className="lastname" type="input" placeholder="Last Name" />
       <input className="age" type="input" placeholder="Age" />
