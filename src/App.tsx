e87da9511f8fa8eaf436010e53d81c6a08ca6840
haelina <56834622+haelina@users.noreply.github.com>
2022-02-14 18:14:20 +0200

    Fix table updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AddPersonForm from "./components/AddPersonForm";
 import Person from "./components/Person";
 import TableComponent from "./components/TableComponent";
 
 const App: React.FC = () => {
-  const data: Person[] = [
+  const [data, setData] = useState<Person[]>([
     { id: 1, firstName: "Jukka", lastName: "Virtanen", age: 30 },
     { id: 2, firstName: "Minna", lastName: "Kuula", age: 22 },
     { id: 3, firstName: "Neea", lastName: "Lattu", age: 27 },
-  ];
+  ]);
 
   const handleAdd = (e: React.FormEvent, person: Person) => {
     e.preventDefault();
     console.log("Handling add new person in App component");
     console.log(`${person.firstName} ${person.lastName} ${person.age}`);
-    data.push(person);
+    setData([...data, person]);
   };
 
   useEffect(() => {
     console.log("data changed.", data.length);
-  }, [data.length]);
+  }, [data]);
 
   return (
     <div className="App">
