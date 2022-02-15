64f785fa61705a91f0b2dc32b1c8bccccb4f2589
haelina <56834622+haelina@users.noreply.github.com>
2022-02-15 15:27:04 +0200

    Add modify function to tablecomponent

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
 
 interface TableProps {
   data: Person[];
+  handleDelete: (p: Person) => void;
 }
 
-const TableComponent: React.FC<TableProps> = ({ data }) => {
+const TableComponent: React.FC<TableProps> = ({ data, handleDelete }) => {
   const columnNames: string[] = ["First name", "Last name", "Age"];
+  const [editingPerson, setEditingPerson] = useState<Person | null>(null);
 
   return (
     <table>
@@ -19,19 +24,49 @@ const TableComponent: React.FC<TableProps> = ({ data }) => {
       </thead>
       <tbody>
         {data.map((person: Person) => {
-          return (
-            <tr key={person.id}>
-              <td>{person.firstName}</td>
-              <td>{person.lastName}</td>
-              <td>{person.age}</td>
-              <td>
-                <button>Modify</button>
-              </td>
-              <td>
-                <button>X</button>
-              </td>
-            </tr>
-          );
+          if (editingPerson !== person) {
+            return (
+              <tr key={person.id}>
+                <td>{person.firstName}</td>
+                <td>{person.lastName}</td>
+                <td>{person.age}</td>
+                <td>
+                  <button onClick={() => setEditingPerson(person)}>
+                    <EditIcon />
+                  </button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(person)}>
+                    <DeleteForeverIcon />
+                  </button>
+                </td>
+              </tr>
+            );
+          } else {
+            return (
+              <tr key={person.id}>
+                <td>
+                  <input type="input" value={person.firstName}></input>
+                </td>
+                <td>
+                  <input value={person.lastName}></input>
+                </td>
+                <td>
+                  <input value={person.age}></input>
+                </td>
+                <td>
+                  <button onClick={() => setEditingPerson(null)}>
+                    <SaveIcon />
+                  </button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(person)}>
+                    <DeleteForeverIcon />
+                  </button>
+                </td>
+              </tr>
+            );
+          }
         })}
       </tbody>
     </table>
