3601bc893050481b022048335dcc30ba081be8fd
haelina <56834622+haelina@users.noreply.github.com>
2022-02-16 17:39:25 +0200

    Change fake data to real data from db

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import "./App.css";
 import AddPersonForm from "./components/AddPersonForm";
 import Person from "./components/Person";
 import TableComponent from "./components/TableComponent";
+import database from "./DBConnection";
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Person[]>([
+  /*const [data, setData] = useState<Person[]>([
     { id: 1, firstName: "Jukka", lastName: "Virtanen", age: 30 },
     { id: 2, firstName: "Minna", lastName: "Kuula", age: 22 },
     { id: 3, firstName: "Neea", lastName: "Lattu", age: 27 },
-  ]);
+  ]);*/
+  const [data, setData] = useState<Person[]>([]);
   const [nextId, setNextId] = useState<number>(data.length + 1);
 
   const handleAdd = (e: React.FormEvent, person: Person) => {
@@ -22,6 +24,16 @@ const App: React.FC = () => {
     const updatedData = data.filter((obj) => obj !== person);
     setData(updatedData);
   };
+
+  const getData = async () => {
+    const d = await database.getAll();
+    console.log(d);
+    setData(d);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   useEffect(() => {
     console.log(data);
